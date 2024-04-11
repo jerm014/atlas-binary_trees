010395d4a262d804e5ab7efc1a76fbc7710af4d2
jerm014 <jeremy.mitts@atlasschool.com>
2024-04-11 02:19:15 +0000

    initial commit

--- 10-main.c
+++ 10-main.c
@@ -1,5 +1,5 @@
 #include "binary_trees.h"
-#include "2-binary_tree_insert_right.c"
+#include "10-binary_tree_depth.c"
 
 /**
  * main - Entry point
@@ -9,14 +9,20 @@
 int main(void)
 {
 	binary_tree_t *root;
+	size_t depth;
 
 	root = binary_tree_node(NULL, 98);
 	root->left = binary_tree_node(root, 12);
 	root->right = binary_tree_node(root, 402);
-	binary_tree_print(root);
-	printf("\n");
 	binary_tree_insert_right(root->left, 54);
 	binary_tree_insert_right(root, 128);
 	binary_tree_print(root);
+
+	depth = binary_tree_depth(root);
+	printf("Depth of %d: %lu\n", root->n, depth);
+	depth = binary_tree_depth(root->right);
+	printf("Depth of %d: %lu\n", root->right->n, depth);
+	depth = binary_tree_depth(root->left->right);
+	printf("Depth of %d: %lu\n", root->left->right->n, depth);
 	return (0);
 }
