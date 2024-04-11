cd483e6c906fc130daecb88a9d0dfa1040e7e56c
jerm014 <jeremy.mitts@atlasschool.com>
2024-04-11 19:09:49 +0000

    first attempt at 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,5 +9,11 @@
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	return (0);
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
+	return (new);
 }
