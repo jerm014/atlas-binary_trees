b6565388c93670616e021b3b7cac20e41b4e5f43
jerm014 <jeremy.mitts@atlasschool.com>
2024-04-11 23:59:30 +0000

    final for today

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,7 +15,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (new == NULL)
 		return (NULL);
 
-	new->parent = parent;
 	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+
 	return (new);
 }
