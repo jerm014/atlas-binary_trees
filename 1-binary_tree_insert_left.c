da24c7b6ba3e30ed27f014405605deb0a25ca24e
jerm014 <jeremy.mitts@atlasschool.com>
2024-04-13 04:33:31 +0000

    removed unused variable

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,7 +10,7 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new, *temp;
+	binary_tree_t *new;
 
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
