0f2207aacf33049f15b77b509b9d8f0893f89e0a
jerm014 <jeremy.mitts@atlasschool.com>
2024-04-13 05:48:38 +0000

    initial commit for 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,5 +9,17 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	return (0);
+	size_t height_l = 0;
+	size_t height_r = 0;
+
+	if (tree->left)
+		height_l = 1 + binary_tree_height(tree->left);
+	
+	if (tree->right)
+		height_r = 1 + binary_tree_height(tree->right);
+
+	if (height_l > height_r)
+		return (height_l);
+	else
+		return (height_r);
 }
