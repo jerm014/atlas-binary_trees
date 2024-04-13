0f4dc9337bdf1b232b80959ee8ec7e0b9f2077dd
jerm014 <jeremy.mitts@atlasschool.com>
2024-04-13 05:14:06 +0000

    initial commit for 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,5 +10,22 @@
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	return (0);
+	binary_tree_t *new;
+
+	if (!parent)
+		return (NULL);
+
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->right)
+	{
+		new->right = parent->right;
+		parent->right->parent = new;
+	}
+	parent->right = new;
+
+	return (new);
+
 }
