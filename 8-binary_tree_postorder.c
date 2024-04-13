046c46ae07ef6f1cbec91d0baa4981f463951dda
jerm014 <jeremy.mitts@atlasschool.com>
2024-04-13 05:43:08 +0000

    initial commit for 8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -12,5 +12,13 @@
 */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	return (0);
+	if (tree)
+	{
+		if (tree->left)
+			binary_tree_postorder(tree->left, func);
+		if (tree->right)
+			binary_tree_postorder(tree->right, func);
+		if (func)
+			func(tree->n);
+	}
 }
