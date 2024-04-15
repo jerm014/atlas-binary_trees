6769075fdb09065d00e74c34f25b199eed8129e0
jerm014 <jeremy.mitts@atlasschool.com>
2024-04-15 03:01:33 +0000

    initial commit for 16

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -28,5 +28,5 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (l && r)
 		return (1);
 
-	return (0); /* error */
+	return (0);
 }
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -21,12 +21,12 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left != NULL && tree->right == NULL)
-		return (1);
+		return (0);
 
 	l = binary_tree_is_perfect(tree->left);
 	r = binary_tree_is_perfect(tree->right);
 	if (l && r)
 		return (1);
-	
+
 	return (0);
 }
