772fd77f696ac14e4a51c3653007daf12ac2a75f
jerm014 <jeremy.mitts@atlasschool.com>
2024-04-16 03:51:43 +0000

    removed trailing whitespace

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -17,9 +17,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 	if (node->parent->left == node)
 		return (node->parent->right);
-	
+
 	if (node->parent->right == node)
 		return (node->parent->left);
-	
+
 	return (NULL);
 }
