f7e3f0c837991b9d7e58b68466e04f6e6bb9d245
jerm014 <jeremy.mitts@atlasschool.com>
2024-04-16 03:56:57 +0000

    initial commit for 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,5 +10,20 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	return (0);
+	if (!node)
+		return (NULL);
+
+	if (!node->parent)
+		return (NULL);
+
+	if (!node->parent->parent)
+		return (NULL);
+
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+
+	if (node->parent->parent->right == node->parent)
+		return (node->parent->parent->left);
+
+	return (NULL);
 }
