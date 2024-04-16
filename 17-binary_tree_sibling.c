0dcf4083823a1c71dff21f2c34bdeaa73fd85907
jerm014 <jeremy.mitts@atlasschool.com>
2024-04-16 03:50:31 +0000

    initial commit for 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,5 +10,16 @@
 */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	return (0);
+	if (!node)
+		return (NULL);
+	if (!node->parent)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+	
+	if (node->parent->right == node)
+		return (node->parent->left);
+	
+	return (NULL);
 }
